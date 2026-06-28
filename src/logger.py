68342505624732eea38037db3abbc5516485f873
Simone12365 <Simone12365@users.noreply.github.com>
2026-06-28 18:32:38 +0200

    Aggiunge modulo di logging centrallizato

---
+++ src/logger.py
@@ -0,0 +1,35 @@
+import logging
+from datetime import datetime
+
+def setup_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    
+    if logger.hasHandlers():
+        return logger
+    
+    log_dir = "logs"
+    import os
+    os.makedirs(log_dir, exist_ok=True)
+    
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_file = f"{log_dir}/pipeline_{timestamp}.log"
+
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+    
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+    
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    logger.setLevel(logging.DEBUG)
+    
+    return logger
